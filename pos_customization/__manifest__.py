80c29a7d4617b8e97f2a65057b24bc0824570715
Htet Aung Shane <mmc.htetaungshane@gmail.com>
2024-04-11 22:01:57 +0630

    update the pos customization with ir_config_parameter

--- pos_customization/__manifest__.py
+++ pos_customization/__manifest__.py
@@ -14,12 +14,13 @@
     "auto_install": False,
     "application": True,
     "data": [
+        'security/ir.model.access.csv',
         "views/res_partner_ext.xml",
         "views/res_config_ext.xml",
     ],
     'assets': {
    'point_of_sale._assets_pos': [
-       'pos_customization/static/src/**/*',
+       'pos_customization/static/src/xml/**/*',
    ],
 },
 }
--- pos_customization/models/pos_slip_customization.py
+++ pos_customization/models/pos_slip_customization.py
@@ -3,3 +3,21 @@ from odoo import fields, models, api, _
 class InheritPOSConfiguration(models.TransientModel):
     _inherit = 'res.config.settings'
     slip_logo = fields.Binary()
+
+    def set_values(self):
+        super(InheritPOSConfiguration, self).set_values()
+        # Update the slip logo image in the settings
+        self.env['ir.config_parameter'].set_param('pos_customization.slip_logo', self.slip_logo)
+
+    @api.model
+    def get_values(self):
+        res = super(InheritPOSConfiguration, self).get_values()
+        # Retrieve the slip logo image from the settings
+        slip_logo = self.env['ir.config_parameter'].sudo().get_param('pos_customization.slip_logo')
+        res.update(slip_logo=slip_logo)
+        return res
+
+class POSImage(models.Model):
+    _name = "blazon.pos.image"
+
+    slip_logo = fields.Binary()
